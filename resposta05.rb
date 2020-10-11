9478697789f15785ec142c238cb20acba8519fb7
Erico Costa <prof.ericocosta@gmail.com>
2020-10-11 11:18:23 -0300

    segundo commit da atividade 06

--- resposta05.rb
+++ resposta05.rb
@@ -4,3 +4,59 @@ A classe deve possuir os seguintes atributos: número da conta, nome do correnti
 Os métodos são os seguintes: alterarNome, depósito e saque;
 No construtor, saldo é opcional, com valor default zero e os demais atributos são obrigatórios.
 =end
+class ContaCorrente
+  attr_accessor :nome_correntista, :numero_conta_corrente, :saldo
+
+  def initialize (numero_conta, nome_correntista, saldo = 0)
+    @numero_conta_corrente = numero_conta
+    @nome_correntista = nome_correntista
+    @saldo = saldo
+  end
+  def alterar_nome (nome)
+    @nome_correntista = nome
+  end
+
+  def deposito (valor)
+    @saldo += valor
+  end
+
+  def saque (valor)
+    @saldo -= valor
+  end
+
+  def to_s
+    "Dados da Conta: #{@numero_conta_corrente}
+      Nome do Correntista: #{@nome_correntista}
+      Saldo: R$ #{'%.2f' % @saldo}"
+  end
+end
+
+print 'Informe o Número da Conta: '
+numero_conta = gets.to_i
+print 'Informe o nome do Correntista: '
+nome = gets.chomp.upcase
+print 'Informe o saldo inicial: '
+saldo = gets.to_f
+correntista = ContaCorrente.new numero_conta, nome, saldo
+puts correntista.to_s
+
+print 'Informe o novo nome para o Correntista: '
+nome = gets.chomp.upcase
+correntista.alterar_nome nome
+puts correntista.to_s
+
+print 'Deseja fazer (D)epósito ou (R)etirada? '
+opcao = gets.chomp.downcase
+
+case opcao
+when 'd'
+  print 'Informe o valor do deposito: '
+  valor = gets.to_s
+  correntista.deposito valor
+when 'r'
+  print 'Informe o valor do saque: '
+  valor = gets.to_s
+  correntista.saque valor
+else
+  puts 'Opção INVÁLIDA'
+end
--- resposta06.rb
+++ resposta06.rb
@@ -3,3 +3,32 @@ Classe TV: Faça um programa que simule um televisor criando-o como um objeto.
 O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume.
 Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas.
 =end
+
+class Televisor
+  attr_accessor :canal, :volume
+
+  def initialize(canal = 4, volume = 15)
+    @canal = canal
+    @volume = volume
+  end
+
+  def trocar_canal(canal)
+    @canal = canal
+  end
+
+  def aumentar_volume
+    @volume >99 ? @volume += 1 : @volume
+  end
+
+  def diminuir_volume
+    @volume > 0 ? @volume -= 1 : @volume
+  end
+
+  def to_s
+    "Canal: #{@canal}
+Volume: #{@volume}"
+  end
+end
+
+Samsung = Televisor.new
+puts Samsung.to_s

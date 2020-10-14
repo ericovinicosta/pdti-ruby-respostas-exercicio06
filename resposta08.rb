12dba7d92d5deed333c0076aaab466b17c3ef676
Erico Costa <prof.ericocosta@gmail.com>
2020-10-14 02:11:01 -0300

    quarto commit da atividade 06

--- resposta08.rb
+++ resposta08.rb
@@ -1,9 +1,45 @@
 =begin
-Classe Macaco: Desenvolva uma classe Macaco,que possua os atributos nome e bucho (estomago) e pelo menos os métodos comer(), verBucho() e digerir().
+Classe Macaco: Desenvolva uma classe Macaco,que possua os atributos nome e bucho (estomago)
+e pelo menos os métodos comer(), verBucho() e digerir().
 Faça um programa ou teste interativamente, criando pelo menos dois macacos,
 alimentando-os com pelo menos 3 alimentos diferentes e verificando o conteúdo do estomago a cada refeição.
 Experimente fazer com que um macaco coma o outro. É possível criar um macaco canibal?
 =end
 class Macaco
+  attr_accessor :nome, :bucho
 
+  def initialize(nome)
+    @nome = nome
+  end
+  def comer(comida)
+    @bucho = comida
+  end
+  def ver_bucho
+    "Aqui tem #{@bucho}"
+  end
+  def digerir
+    @bucho = ''
+  end
+  def to_s
+    "Meu nome é #{@nome}"
+  end
 end
+
+puts 'Alimente os macacos..'
+puts 'Dê nome aos macacos'
+print 'Qual o nome do primeiro: '
+macaco_1 = Macaco.new gets.chomp
+print  'Qual o nome do segundo: '
+macaco_2 = Macaco.new gets.chomp
+puts macaco_1.to_s
+puts macaco_2.to_s
+cont = 0
+begin
+  cont += 1
+  print 'Que comida deseja dar: '
+  macaco_1.comer gets.chomp
+  puts macaco_1.ver_bucho
+  macaco_1.digerir
+end while cont < 3
+macaco_2.comer macaco_1
+puts macaco_2.ver_bucho
--- resposta09.rb
+++ resposta09.rb
@@ -14,13 +14,39 @@
 =end
 class Ponto
   attr_accessor :x, :y
-
+  def initialize(x=0, y=0)
+    @x = x
+    @y = y
+  end
   def to_s
-    "#{self} (#{@x},#{@y})"
+    "(#{@x},#{@y})"
   end
 end
 
 class Retangulo
-  attr_accessor :largura, :altura
+  attr_accessor :largura, :altura, :ponto_inicial
+  def initialize(largura, altura, x = 0 , y = 0)
+    @largura = largura
+    @altura = altura
+    @ponto_inicial = Ponto.new x, y
+  end
+  def ponto_centro
+    centro = Ponto.new (@largura / 2) + @ponto_inicial.x, (@altura / 2) + @ponto_inicial.y
+  end
+end
 
+def mudar_valores (retangulo, altura, largura)
+  retangulo.largura = largura
+  retangulo.altura = altura
+end
+
+retangulo = Retangulo.new 10, 13
+ponto = Ponto.new retangulo.ponto_centro.x, retangulo.ponto_centro.y
+begin
+  print 'Entre com a nova altura: '
+  retangulo.altura = gets.to_i
+  print 'Entro com a nova largura: '
+  retangulo.largura = gets.to_i
+  ponto = retangulo.ponto_centro
+  puts "O ponto central é: #{ponto.to_s}"
 end
